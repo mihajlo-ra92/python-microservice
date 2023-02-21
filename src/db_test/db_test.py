3eea7e8ea480b3c0c476e17a606fd28a953e3c3e
mihajlo-ra92 <maksimovic.ra92.2019@uns.ac.rs>
2023-02-21 14:09:15 +0100

    Code cleanup

--- src/db_test/db_test.py
+++ src/db_test/db_test.py
@@ -1,41 +1,43 @@
 import os, uuid, json
-from flask import Flask, request, jsonify
+from flask import Flask, request
 from flask_mysqldb import MySQL
+
 server = Flask(__name__)
 
-server.config['MYSQL_HOST'] = 'mysql'
-server.config['MYSQL_USER'] = 'root'
-server.config['MYSQL_PASSWORD'] = 'password'
-server.config['MYSQL_DB'] = 'users'
+# TODO: read env_vars not  regular string
+server.config["MYSQL_HOST"] = "mysql"
+server.config["MYSQL_USER"] = "root"
+server.config["MYSQL_PASSWORD"] = "password"
+server.config["MYSQL_DB"] = "users"
 
 mysql = MySQL(server)
 
-@server.route('/users', methods=["GET"])
+
+@server.route("/users", methods=["GET"])
 def index():
-  cur = mysql.connection.cursor()
-  db_res = cur.execute(f"SELECT * FROM Users")
-  row_headers=[x[0] for x in cur.description] #this will extract row headers
-  if db_res > 0:
-    user_details = cur.fetchall() 
-  cur.close()
-  json_data=[]
-  for result in user_details:
-    json_data.append(dict(zip(row_headers,result)))
-  return json.dumps(json_data)
+    cur = mysql.connection.cursor()
+    _ = cur.execute(f"SELECT * FROM Users")
+    user_details = cur.fetchall()
+    cur.close()
+    return json.dumps(user_details)
 
-@server.route('/add-user', methods=["POST"])
+
+@server.route("/add-user", methods=["POST"])
 def add_user():
-  user_details = request.json
-  print(f"user_details: {user_details}")
-  username = user_details["username"]
-  password = user_details["password"]
-  email = user_details["email"]
+    user_details = request.json
+    username = user_details["username"]
+    password = user_details["password"]
+    email = user_details["email"]
 
-  cur = mysql.connection.cursor()
-  print(f"INSERT INTO Users(id, username, password, email) VALUES ('{str(uuid.uuid1())}', '{username}', '{password}', '{email}');")
-  cur.execute(f"INSERT INTO Users(id, username, password, email) VALUES ('{str(uuid.uuid1())}', '{username}', '{password}', '{email}');")
-  mysql.connection.commit()
-  cur.close()
-  return 'success'
+    cur = mysql.connection.cursor()
+    cur.execute(
+        f"INSERT INTO Users(id, username, password, email) VALUES \
+        ('{str(uuid.uuid1())}', '{username}', '{password}', '{email}');"
+    )
+    mysql.connection.commit()
+    cur.close()
+    return "success"
+
+
 if __name__ == "__main__":
     server.run(host="0.0.0.0", port=5000)
