fa283b086b4996e0f1ab4799b16625204930846f
mihajlo-ra92 <maksimovic.ra92.2019@uns.ac.rs>
2023-02-22 17:14:11 +0100

    Implement jwt generation and requirement

--- src/auth/auth.py
+++ src/auth/auth.py
@@ -1,3 +1,5 @@
+from dataclasses import dataclass
+from functools import wraps
 import requests, json, jwt, datetime
 from flask import Flask, jsonify, request
 from logging.config import dictConfig
@@ -25,14 +27,41 @@ app = Flask(__name__)
 app.config["SECRET_KEY"] = "verysecret"
 
 
+@dataclass
+class UserData:
+    username: str
+    user_type: str
+
+
+def token_required(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        token = request.headers.get(
+            "Bearer"
+        )  # http://localhost:5002/route?token=eydsafdsaf
+        app.logger.info(token)
+        if not token:
+            return json.dumps({"text": "Token is missing"}), 401
+        try:
+            data = jwt.decode(token, app.config["SECRET_KEY"], algorithms=["HS256"])
+        except Exception as inst:
+            app.logger.info(inst)
+            return json.dumps({"text": "Token is missing or invalid"}), 401
+        user_data = UserData(data["user"], data["user_type"])
+        return f(user_data, *args, **kwargs)
+
+    return decorated
+
+
 @app.route("/unprotected")
 def unprotected():
     return "unprotected"
 
 
 @app.route("/protected")
-def protected():
-    return "protected"
+@token_required
+def protected(user_data):
+    return f"user_data is: {user_data}"
 
 
 @app.route("/login", methods=["POST"])
@@ -51,11 +80,12 @@ def login():
     token = jwt.encode(
         {
             "user": username,
+            "user_type": req.json()["user_type"],
             "exp": datetime.datetime.utcnow() + datetime.timedelta(hours=24),
         },
         app.config["SECRET_KEY"],
     )
-    return json.dumps({"token": token}), 201
+    return json.dumps({"Bearer": token}), 201
 
 
 if __name__ == "__main__":
