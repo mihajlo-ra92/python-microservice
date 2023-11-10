82c032641df00897d7d506f24b36bad3ba88fd22
mihajlo-ra92 <mihajlo2000@gmail.com>
2023-11-10 23:36:47 +0100

    Connect jaeger to user service

--- src/users/user_controller.py
+++ src/users/user_controller.py
@@ -7,19 +7,23 @@ from opentelemetry import trace
 from opentelemetry.sdk.trace import TracerProvider
 from opentelemetry.sdk.trace.export import (
     BatchSpanProcessor,
-    ConsoleSpanExporter,
 )
 from opentelemetry.trace import Status, StatusCode
+from opentelemetry.exporter.otlp.proto.grpc.trace_exporter import OTLPSpanExporter
+from opentelemetry.sdk.resources import Resource
 
-provider = TracerProvider()
-processor = BatchSpanProcessor(ConsoleSpanExporter())
-provider.add_span_processor(processor)
+# Resource can be required for some backends, e.g. Jaeger
+# If resource wouldn't be set - traces wouldn't appears in Jaeger
+resource = Resource(attributes={"service.name": "user_service"})
 
-# Sets the global default tracer provider
-trace.set_tracer_provider(provider)
+trace.set_tracer_provider(TracerProvider(resource=resource))
+tracer = trace.get_tracer("user_service")
 
-# Creates a tracer from the global tracer provider
-tracer = trace.get_tracer("user_tracer")
+otlp_exporter = OTLPSpanExporter(endpoint="http://jaeger:4317", insecure=True)
+
+span_processor = BatchSpanProcessor(otlp_exporter)
+
+trace.get_tracer_provider().add_span_processor(span_processor)
 
 
 set_logger_config()
@@ -48,7 +52,9 @@ def init_test_db():
 
 @app.route("/users/read-all", methods=["GET"])
 def read_all():
-    with tracer.start_as_current_span("/users/read-all") as span:
+    print("jeager env:")
+    print(os.environ.get("JEAGER_HOST_PORT"))
+    with tracer.start_as_current_span("[GET] /users/read-all") as span:
         users: list[User] = service.read_all()
         span.set_status(Status(StatusCode.OK))
         return json.dumps(users), 200
