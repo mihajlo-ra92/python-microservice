f0c6da26ca54ce9c855d00f0244056f1196b3af4
mihajlo-ra92 <mihajlo2000@gmail.com>
2023-12-30 15:04:15 +0100

    Cleanup users component

--- src/vite-project/src/components/Protected.jsx
+++ src/vite-project/src/components/Protected.jsx
@@ -1,6 +1,5 @@
 import { useNavigate, useLocation } from "react-router-dom";
 import useAxiosPrivate from "../hooks/useAxiosPrivate";
-import useAuth from "../hooks/useAuth";
 import { useEffect, useState } from "react";
 
 const Users = () => {
@@ -8,26 +7,20 @@ const Users = () => {
   const axiosPrivate = useAxiosPrivate();
   const navigate = useNavigate();
   const location = useLocation();
-  const auth = useAuth();
-  console.log(auth);
 
   useEffect(() => {
     let isMounted = true;
     const controller = new AbortController();
-    console.log("users2");
 
     const getUsers = async () => {
       try {
-        console.log("before");
         // const response = await axiosPrivate.get("/users/read-logged-in", {
         //   signal: controller.signal,
         // });
         const response = await axiosPrivate.get("/users/read-logged-in");
-        console.log("after");
+        console.log("res");
         console.log(response.data);
         isMounted && setUsers(response.data);
-        console.log("users");
-        console.log(users);
       } catch (err) {
         console.error(err);
         navigate("/login", { state: { from: location }, replace: true });
@@ -40,18 +33,16 @@ const Users = () => {
       isMounted = false;
       controller.abort();
     };
-  }, []);
+  }, [axiosPrivate, location, navigate]);
 
   return (
     <>
       <p>Works well.</p>
       <article>
         <h2>Users List</h2>
-        {users?.length ? (
+        {users ? (
           <ul>
-            {users.map((user, i) => (
-              <li key={i}>{user?.username}</li>
-            ))}
+            <li key={1}>{users?.username}</li>
           </ul>
         ) : (
           <p>No users to display</p>
